d2809b4a6a0e685690344a6887fef2fb1ad14326
Herin Kim <herin@hyperlootproject.com>
2023-04-03 21:07:03 +0700

    feat(metadata): setup metadata for home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,14 @@
 		<PagesHomeSignUp />
 	</div>
 </template>
+
+<script setup>
+useServerSeoMeta({
+  title: 'CC0 Wars | HyperLoot',
+  ogTitle: 'CC0 Wars | HyperLoot',
+  description: '',
+  ogDescription: '',
+  ogImage: '',
+  twitterCard: 'summary_large_image',
+});
+</script>
