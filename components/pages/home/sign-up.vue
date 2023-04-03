f4615ba35570c4628ee504b36ec1b680c69c3156
Herin Kim <herin@hyperlootproject.com>
2023-04-03 16:26:59 +0700

    feat(signup): implement signup form with temp api call

---
+++ components/pages/home/sign-up.vue
@@ -0,0 +1,36 @@
+<template>
+	<section class="sign-up">
+		<h2>Sign Up</h2>
+		<form method="POST" @submit.prevent="onSignUpSubmit">
+			<label for="email">Email:</label>
+			<input type="text" name="email" placeholder="hello@cc0wars.com" required v-model="email">
+			<button type="submit">Sign Up</button>
+		</form>
+		<span>{{ email }}</span>
+	</section>
+</template>
+
+<script setup>
+	const email = ref('');
+
+	const onSignUpSubmit = (() => {
+		console.log('submit');
+
+		signUpformReqeust().then((response) => {
+			console.log(response);
+		});
+	});
+
+	const signUpformReqeust = (async () => {
+		console.log('formReqeust');
+		return await $fetch('http://localhost:8000/signup', { 
+            headers: {
+				"Content-Type": "application/json",
+            },
+            method: 'POST',
+            body: {
+                email: email.value,
+            },
+        });
+	});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
 		test
+		<PagesHomeSignUp/>
 	</div>
 </template>
