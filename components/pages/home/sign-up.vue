61673afd84f67b6d0b2a2af1aabb50a1d4095a73
Herin Kim <herin@hyperlootproject.com>
2023-04-03 17:14:32 +0700

    feat(signup): set api base in config

--- components/pages/home/sign-up.vue
+++ components/pages/home/sign-up.vue
@@ -3,34 +3,50 @@
 		<h2>Sign Up</h2>
 		<form method="POST" @submit.prevent="onSignUpSubmit">
 			<label for="email">Email:</label>
-			<input type="text" name="email" placeholder="hello@cc0wars.com" required v-model="email">
+			<input type="text" name="email" placeholder="hello@cc0wars.com" required v-model.trim="email" @input="resetForm">
 			<button type="submit">Sign Up</button>
 		</form>
-		<span>{{ email }}</span>
+		<div v-if="isUserSubmitted && !isEmailValid">Please enter a valid email.</div>
 	</section>
 </template>
 
 <script setup>
+	const config = useRuntimeConfig();
+
 	const email = ref('');
+	const isEmailValid = ref(false);
+	const isUserSubmitted = ref(false);
+
+	const validateEmail = (() => {
+		const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		isEmailValid.value = re.test(email.value);
+	});
 
 	const onSignUpSubmit = (() => {
-		console.log('submit');
+		validateEmail();
 
-		signUpformReqeust().then((response) => {
-			console.log(response);
-		});
+		isUserSubmitted.value = true;
+
+		if (isEmailValid.value) {
+			signUpformReqeust().then((response) => {
+				console.log(response);
+			});
+		}
 	});
 
 	const signUpformReqeust = (async () => {
-		console.log('formReqeust');
-		return await $fetch('http://localhost:8000/signup', { 
+		return await $fetch(`${config.public.hyperloot.apiBase}/signup`, { 
             headers: {
-				"Content-Type": "application/json",
+				'Content-Type': 'application/json',
             },
             method: 'POST',
             body: {
                 email: email.value,
             },
         });
+	});
+
+	const resetForm = (() => {
+		isUserSubmitted.value = false;
 	});
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-})
+	runtimeConfig: {
+		public: {
+			hyperloot: {
+				apiBase: '',
+			},
+		}
+	},
+});
