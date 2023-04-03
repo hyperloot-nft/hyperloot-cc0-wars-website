8cb2660552be5951b41659b49d329808eec4e6ad
Herin Kim <herin@hyperlootproject.com>
2023-04-03 19:31:15 +0700

    feat(signup): implement api hanlder

--- components/pages/home/sign-up.vue
+++ components/pages/home/sign-up.vue
@@ -6,7 +6,9 @@
 			<input type="text" name="email" placeholder="hello@cc0wars.com" required v-model.trim="email" @input="resetForm">
 			<button type="submit">Sign Up</button>
 		</form>
-		<div v-if="isUserSubmitted && !isEmailValid">Please enter a valid email.</div>
+		<div v-if="isFormSubmitted && !isEmailValid">Please enter a valid email.</div>
+		<div v-if="isFormSubmitted && isFormSubmitSuccess">Thank you.</div>
+		<div v-else-if="isFormSubmitted && !isFormSubmitSuccess && isEmailValid">Sorry, something went wrong. Please try again later.</div>
 	</section>
 </template>
 
@@ -15,7 +17,8 @@
 
 	const email = ref('');
 	const isEmailValid = ref(false);
-	const isUserSubmitted = ref(false);
+	const isFormSubmitted = ref(false);
+	const isFormSubmitSuccess = ref(false);
 
 	const validateEmail = (() => {
 		const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -25,12 +28,16 @@
 	const onSignUpSubmit = (() => {
 		validateEmail();
 
-		isUserSubmitted.value = true;
+		isFormSubmitted.value = true;
 
 		if (isEmailValid.value) {
 			signUpformReqeust().then((response) => {
+				isFormSubmitSuccess.value = true;
 				console.log(response);
-			});
+			}).catch((error) => {
+				console.log(error);
+				isFormSubmitSuccess.value = false;
+			})
 		}
 	});
 
@@ -47,6 +54,7 @@
 	});
 
 	const resetForm = (() => {
-		isUserSubmitted.value = false;
+		isFormSubmitted.value = false;
+		isFormSubmitSuccess.value = false;
 	});
 </script>
