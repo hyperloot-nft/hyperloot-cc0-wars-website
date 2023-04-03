816f1b536bcf206c7adee0fa2310c474338604de
Thanakron Tandavas <tandavas@famolus.com>
2023-04-03 19:25:12 +0700

    feat(home): add hero bg images with nuxt image

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	modules: [
+    	'@nuxt/image-edge',
+	],
 	vite: {
 		css: {
 			preprocessorOptions: {
 				scss: {
-					additionalData: '@use "@/assets/styles/global.scss" as global;'
+					additionalData: '@use "@/assets/styles/global.scss" as global;',
 				}
 			}
 		}
