020e8ba4390956c75df10ebac1c8d540da4e91d6
Thanakron Tandavas <tandavas@famolus.com>
2023-04-04 19:29:45 +0700

    feat(info): style info section

--- components/pages/home/info.vue
+++ components/pages/home/info.vue
@@ -2,18 +2,16 @@
 	<section class="info">
 		<div class="container">
 			<div class="tagline">
-				<h2>Community-owned.</h2>
-				<h2>Team-based multiplayer.</h2>
-				<h2>Fast-paced action strategy.</h2>
+				<h2>Community-owned.<br>Team-based multiplayer.<br>Fast-paced action strategy.</h2>
 			</div>
-			<div class="work-contact">
+			<div class="work">
 				<div class="partner">
-					<div>Streamer and influencer partnership</div>
-					<div>creator@cc0wars.com</div>
+					<div class="title">Streamer and influencer partnership</div>
+					<div class="email"><a href="mailto:creator@cc0wars.com">creators@cc0wars.com</a></div>
 				</div>
 				<div class="business">
-					<div>Business inquiry</div>
-					<div>business@cc0wars.com</div>
+					<div class="title">Business inquiry</div>
+					<div class="email"><a href="mailto:business@cc0wars.com">business@cc0wars.com</a></div>
 				</div>
 			</div>
 		</div>
@@ -24,6 +22,20 @@
 $top-spacing: 11.25rem;
 $bottom-spacing: 11.25rem;
 
+$work-border-color: #43444D;
+
+h2 {
+	line-height: global.$font-line-height-sm;
+}
+
+a {
+	color: global.$color-text-dark;
+
+	&:hover {
+		color: global.$color-text;
+	}
+}
+
 .info {
 	margin-top: $top-spacing;
 	margin-bottom: $bottom-spacing;
@@ -31,9 +43,39 @@ $bottom-spacing: 11.25rem;
 
 .container {
 	display: flex;
-	justify-content: space-between;
-	width: global.$container-width;
+	align-items: center;
+	justify-content: center;
+	max-width: global.$container-width;
+	width: 100%;
 	margin-left: auto;
 	margin-right: auto;
+}
+
+.tagline {
+	max-width: 38.75rem;
+	width: 100%;
+	margin-right: global.$grid-gutter-width;
+}
+
+.work {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding-top: 0.25rem;
+	padding-left: global.$grid-gutter-width;
+	height: 10rem;
+	border-left: 1px solid $work-border-color;
+}
+
+.partner {
+	margin-bottom: 1rem;
+}
+
+.title {
+	font-size: global.$font-size-xs;
+}
+
+.email {
+	font-size: global.$font-size-xs;
 }
 </style>
