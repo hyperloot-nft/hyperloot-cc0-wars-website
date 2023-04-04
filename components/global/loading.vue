bacf7723fdd91f56eb7d0645ee8ea5ce0d9b6671
Thanakron Tandavas <tandavas@famolus.com>
2023-04-04 22:14:11 +0700

    feat(signup): style loading spinner

--- components/global/loading.vue
+++ components/global/loading.vue
@@ -21,19 +21,20 @@ const props = defineProps({
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	width: 36px;
-    height: 100%;
+	width: 28px;
+	height: 28px;
 }
 
 .spinner {
 	display: inline-block;
 	position: relative;
-	width: 20px;
+	width: 28px;
+	height: 28px;
 }
 
 .spinner div {
 	position: absolute;
-	border: 4px solid global.$color-gray-lightest;
+	border: 4px solid #000;
 	opacity: 1;
 	border-radius: 50%;
 	animation: spinner 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
@@ -45,22 +46,22 @@ const props = defineProps({
 
 @keyframes spinner {
 	0% {
-		top: 18px;
-		left: 18px;
+		top: 14px;
+		left: 14px;
 		width: 0;
 		height: 0;
 		opacity: 0;
 	}
 	4.9% {
-		top: 18px;
-		left: 18px;
+		top: 14px;
+		left: 14px;
 		width: 0;
 		height: 0;
 		opacity: 0;
 	}
 	5% {
-		top: 18px;
-		left: 18px;
+		top: 14px;
+		left: 14px;
 		width: 0;
 		height: 0;
 		opacity: 1;
@@ -68,9 +69,10 @@ const props = defineProps({
 	100% {
 		top: 0px;
 		left: 0px;
-		width: 36px;
-		height: 36px;
+		width: 25px;
+		height: 25px;
 		opacity: 0;
 	}
 }
+
 </style>
