226eb55a6845c3341aab19769d626c2fa95c4047
Herin Kim <herin@hyperlootproject.com>
2023-04-04 20:48:16 +0700

    fix(metadata): wrong img name

--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,8 @@ useServerSeoMeta({
   ogTitle: 'CC0 Wars | HyperLoot',
   description: 'Get ready to join the battle with HyperLoot in CC0 Wars! Coming soon to iOS and Android.',
   ogDescription: 'Get ready to join the battle with HyperLoot in CC0 Wars! Coming soon to iOS and Android.',
-  ogImage: 'https://cc0wars.com/hyperloot-cc0wars-hero-facebook.jpg',
-  twitterImage: 'https://cc0wars.com/hyperloot-cc0wars-hero-twitter.jpg',
+  ogImage: 'https://cc0wars.com/hyperloot-cc0-wars-og-image-facebook.jpg',
+  twitterImage: 'https://cc0wars.com/hyperloot-cc0-wars-og-image-twitter.jpg',
   twitterCard: 'summary_large_image',
 });
 </script>
