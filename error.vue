cdfe08d626f7c639a18f05a9c011295706eff400
Herin Kim <herin@hyperlootproject.com>
2023-04-12 23:58:59 +0700

    feat(404) implement custom 404 page

---
+++ error.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="not-found">
+		<Navbar />
+		<div class="container">
+			<h1>404</h1>
+			<nuxt-link to="/">Go to home</nuxt-link>
+		</div>
+		<Footer />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+
+h1 {
+	font-size: global.$font-size-h1;
+}
+
+.container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	width: 100%;
+	height: 80vh;
+}
+</style>
